840d2916dbf95f9354718f4ff2f8b0bf8d41758d
YannickLeRoux <rasyann@hotmail.com>
2018-10-05 08:21:49 -0700

    Got a first sound to play

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react'
 import { Text, View, Slider, Button } from 'react-native';
 
-import click1 from '../assets/click1.wav';
-import click2 from '../assets/click2.wav';
 
 export default class Metronome extends Component {
-  constructor(props) {
-    super(props)
-
-    state = {
-      bpm: 100,
-      playing: false,
-      count: 0,
-      beatPerMeasure: 4
-    }
-// https://docs.expo.io/versions/v28.0.0/sdk/audio#__next
-//https://github.com/expo/playlist-example/blob/master/App.js
-    this.click1 = new Expo.Audio(click1);
-    this.click2 = new Expo.Audio(click2);
+  state = {
+    bpm: 100,
+    playing: false,
+    count: 0,
+    beatPerMeasure: 4
   }
 
-  onButtonPress() {
-    this.setState({ playing: !this.state.playing })
+  // https://docs.expo.io/versions/v28.0.0/sdk/audio#__next
+  // https://github.com/expo/playlist-example/blob/master/App.js
+  // https://daveceddia.com/react-practice-projects/
+
+  async onButtonPress() {
+    this.setState({ playing: !this.state.playing });
+
+    const soundObject = new Expo.Audio.Sound();
+    try {
+      await soundObject.loadAsync(require('../assets/click1.mp3'));
+      await soundObject.playAsync();
+      // Your sound is playing!
+    } catch (error) {
+      // An error occurred!
+    }
 
   }
 
@@ -52,7 +55,7 @@ export default class Metronome extends Component {
 const styles = {
   container: {
     flex: 1,
-    justifyItems: 'space around',
+    justifyItems: 'spaceAround',
     alignItems: 'center'
   },
   slider : {
